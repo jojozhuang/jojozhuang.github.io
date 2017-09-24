8d3f2e424b479d0a1531ef68c0115543e662a215
Zhuang, Rong <jojozhuang@gmail.com>
2017-09-23 19:32:48 -0700

    library, hover button

---
+++ templates/js/popover.js
@@ -0,0 +1,15 @@
+$(".pop").popover({ trigger: "manual" , html: true, animation:false})
+    .on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide");
+            }
+        }, 300);
+});
