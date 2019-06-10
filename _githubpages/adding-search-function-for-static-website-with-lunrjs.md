5bd069c0f8653e3b4146ab3e68c17138ef5baacf
Zhuang, Rong <jojozhuang@gmail.com>
2019-06-09 20:33:07 -0700

    search function for frontend, java, programming, devops

--- _githubpages/adding-search-function-for-static-website-with-lunrjs.md
+++ _githubpages/adding-search-function-for-static-website-with-lunrjs.md
@@ -19,3 +19,5 @@ tags: [Search, Lunr.js]
 * [Jekyll search using lunr.js](https://learn.cloudcannon.com/jekyll/jekyll-search-using-lunr-js/)
 * [Jekyll Search with Algolia and Webtasks](https://forestry.io/blog/search-with-algolia-in-jekyll/)
 * [Lunr UPGRADING](https://lunrjs.com/guides/upgrading.html)
+* [Jekyll Search Function](http://www.mtlong.me/blog/jekyll-search-function/)
+* [How to strip newlines in jekyll?](https://stackoverflow.com/questions/45147948/how-to-strip-newlines-in-jekyll)
--- public/js/search.js
+++ public/js/search.js
@@ -4,16 +4,15 @@
 
     if (results.length) { // Are there any results?
       var appendString = '';
-
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
-		  console.log(results[i]);
         var item = store[results[i].ref];
-
-		console.log(item);
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+        appendString += '<dd>';
+        appendString += '  <a href="' + item.url + '" title="' + item.title + '">';
+        appendString += '    <div>' + item.index + '. ' + item.title + '</div>';
+        appendString += '  </a>';
+        appendString += '  <div class="pull-left"><p>' + item.excerpt + '</p></div>';
+        appendString += '</dd>';
       }
-
       searchResults.innerHTML = appendString;
     } else {
       searchResults.innerHTML = '<li>No results found</li>';
@@ -36,8 +35,8 @@
   var searchTerm = getQueryVariable('query');
 
   if (searchTerm) {
-    document.getElementById('search-box').setAttribute("value", searchTerm);
-	document.getElementById('search-box').setAttribute("value", searchTerm);
+    //document.getElementById('search-box').setAttribute("value", searchTerm);
+    document.getElementById('search-key').innerHTML = searchTerm;
 
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
@@ -46,21 +45,25 @@
       this.field('title', { boost: 10 });
       //this.field('category');
       this.field('content');
-	  this.field('url');
+      this.field('url');
 
-	  for (var key in window.store) { // Add the data to lunr
+      for (var key in window.store) { // Add the data to lunr
         this.add({
           'id': key,
           'title': window.store[key].title,
           //'author': window.store[key].author,
           //'category': window.store[key].category,
           'content': window.store[key].content,
-          'url': window.store[key].url
+          'url': window.store[key].url,
+          'index': window.store[key].index,
+          'excerpt': window.store[key].excerpt
         });
       }
     });
 
     var results = idx.search(searchTerm); // Get lunr to perform a search
+    console.log("results:");
+    console.log(results);
     displaySearchResults(results, window.store); // We'll write this in the next section
   }
 })();
